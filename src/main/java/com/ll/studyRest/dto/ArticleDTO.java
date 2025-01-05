d19159dd366156db25f483e3cf14179feca11d18
ksuju <139477852+ksuju@users.noreply.github.com>
2025-01-05 17:28:44 +0900

    refactor: ArticleDTO 수정

---
+++ src/main/java/com/ll/studyRest/dto/ArticleDTO.java
@@ -0,0 +1,34 @@
+package com.ll.studyRest.dto;
+
+import com.ll.studyRest.entity.Article;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+/**
+ * packageName    : com.ll.studyRest.dto
+ * fileName       : ArticleDto
+ * author         : sungjun
+ * date           : 2025-01-05
+ * description    : 자동 주석 생성
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2025-01-05        kyd54       최초 생성
+ */
+@Getter
+public class ArticleDTO {
+    private final Long id;
+    private final String subject;
+    private final String content;
+    private final LocalDateTime createdDate;
+    private final LocalDateTime modifiedDate;
+
+    public ArticleDTO(Article article) {
+        this.id = article.getId();
+        this.subject = article.getSubject();
+        this.content = article.getContent();
+        this.createdDate = article.getCreatedDate();
+        this.modifiedDate = article.getModifiedDate();
+    }
+}
