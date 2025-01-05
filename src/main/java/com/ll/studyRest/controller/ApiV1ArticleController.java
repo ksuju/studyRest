8f9286edb611e791364789ec5a3387331c0276ad
ksuju <139477852+ksuju@users.noreply.github.com>
2025-01-05 17:38:19 +0900

    feat:데이터 단건, 다건 출력 테스트

--- src/main/java/com/ll/studyRest/controller/ApiV1ArticleController.java
+++ src/main/java/com/ll/studyRest/controller/ApiV1ArticleController.java
@@ -1,8 +1,13 @@
 package com.ll.studyRest.controller;
 
+import com.ll.studyRest.dto.ArticleDTO;
+import com.ll.studyRest.entity.Article;
 import com.ll.studyRest.service.ArticleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * packageName    : com.ll.studyRest.controller
@@ -22,13 +27,37 @@ public class ApiV1ArticleController {
     private final ArticleService articleService;
 
     @GetMapping("")    // 다건 조회
-    public String list() {
-        return "다건";
+    public List<ArticleDTO> list() {
+        List<ArticleDTO> articleList = new ArrayList<>();
+
+        // 임시 데이터 삽입
+        Article article1 = new Article("제목1", "내용1");
+        articleList.add(new ArticleDTO(article1));
+
+        Article article2 = new Article("제목2", "내용2");
+        articleList.add(new ArticleDTO(article2));
+
+        Article article3 = new Article("제목3", "내용3");
+        articleList.add(new ArticleDTO(article3));
+
+        return articleList;
     }
 
     @GetMapping("/{id}")  // 단건 조회
-    public String getArticle() {
-        return "단건";
+    public ArticleDTO getArticle() {
+        List<ArticleDTO> articleList = new ArrayList<>();
+
+        // 임시 데이터 삽입
+        Article article1 = new Article("제목1", "내용1");
+        articleList.add(new ArticleDTO(article1));
+
+        Article article2 = new Article("제목2", "내용2");
+        articleList.add(new ArticleDTO(article2));
+
+        Article article3 = new Article("제목3", "내용3");
+        articleList.add(new ArticleDTO(article3));
+
+        return articleList.get(0);  // 0번 인덱스에 저장된 데이터 단건 조회
     }
 
     @PostMapping("")   // 생성
